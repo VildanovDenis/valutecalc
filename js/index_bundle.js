4a84ac875a52c7e09565dd6ebda451f94e9d4cb1
VildanovDenis <disamapm@gmail.com>
2019-06-09 14:03:42 +0300

    Makes repo

---
+++ js/index_bundle.js
@@ -0,0 +1,60 @@
+const input = document.querySelector('#value');
+const valuteSelect = document.querySelector('#valute');
+const output = document.querySelector('#new_value');
+let valute = {};
+
+function getValuteData() {
+    const URL = 'https://www.cbr-xml-daily.ru/daily_json.js';
+    const headers = {
+        method: 'GET',
+        mode: 'cors'
+    }
+
+    return fetch(URL, headers)
+            .then(res => res.json())
+            .then(data => data.Valute)
+            .then(valute => valute)
+            .catch(err => console.log(err));
+}
+
+async function createOptions() {
+    valute = await getValuteData();
+    // let selectInnerHtml = '';
+
+    for (key in valute) {
+        // const newStr = `<option title=${valute[key].Name}>${valute[key].CharCode}</option>`;
+        const option = document.createElement('option');
+        option.setAttribute('title', valute[key].Name);
+        option.innerHTML = valute[key].CharCode
+
+        valuteSelect.appendChild(option);
+        // selectInnerHtml = selectInnerHtml + newStr;
+    }
+};
+
+createOptions();
+
+input.addEventListener('input', function(e) {
+    const value = this.value;
+    const valuteCode = valuteSelect.value
+    const outputValue = convertValute(value, valute[valuteCode].Nominal, valute[valuteCode].Value);
+    
+    output.value = outputValue;
+});
+
+valuteSelect.addEventListener('change', function(e) {
+    const valuteCode = this.value;
+    const value = input.value;
+    if (value === '') return;
+    const outputValue = convertValute(value, valute[valuteCode].Nominal, valute[valuteCode].Value);
+
+    output.value = outputValue;
+})
+
+function convertValute(value, valuteNominal, valuteValue) {
+    const converted = (value * valuteNominal / valuteValue).toFixed(2);
+    if (isNaN(converted)) {
+        return 'Введите корректное значние'
+    }
+    return (value * valuteNominal / valuteValue).toFixed(2);
+}
